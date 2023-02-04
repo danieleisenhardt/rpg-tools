87004eb8053d94a24d6609fdc38a0662843939ee
Daniel Eisenhardt <eisenhardtdaniel@gmail.com>
2023-02-04 11:45:54 +0100

    OGL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import {AppComponent} from './app.component';
 import {HitChanceComponent} from './component/hit-chance/hit-chance.component';
 import {HomeComponent} from './component/home/home.component';
 import {SkillVsD100Component} from './component/hit-chance/skill-vs-d100/skill-vs-d100.component';
+import { OglComponent } from './component/hit-chance/ogl/ogl.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HitChanceComponent,
         HomeComponent,
-        SkillVsD100Component
+        SkillVsD100Component,
+        OglComponent
     ],
     imports: [
         BrowserModule,
---
+++ src/app/component/hit-chance/ogl/ogl.component.ts
@@ -0,0 +1,75 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-ogl',
+    templateUrl: './ogl.component.html',
+    styleUrls: ['./ogl.component.css']
+})
+export class OglComponent implements OnInit {
+
+    attackerLevel = 1;
+    attackerStrength = 10;
+    defenderDexterity = 10;
+    defenderArmorClass = 11;
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    getHitChance() {
+        let chance = 100 - this.defenderArmorClass * 5;
+
+        chance += this.calculateProficiencyBonus(this.attackerLevel) * 5;
+        chance += this.calculateAbilityBonus(this.attackerStrength) * 5;
+        chance -= this.calculateAbilityBonus(this.defenderDexterity) * 5;
+
+        return Math.max(5, Math.min(95, chance));
+    }
+
+    calculateAbilityBonus(score: number) {
+        if (score >= 18) {
+            return 3;
+        }
+        if (score >= 16) {
+            return 2;
+        }
+        if (score >= 13) {
+            return 1;
+        }
+        if (score >= 9) {
+            return 0;
+        }
+        if (score >= 6) {
+            return -1;
+        }
+        if (score >= 4) {
+            return -2;
+        }
+        return -3;
+    }
+
+    getAbilityBonusText(score: number) {
+        const bonus = this.calculateAbilityBonus(score);
+
+        return (bonus > -1 ? '+' : '') + bonus;
+    }
+
+    calculateProficiencyBonus(level: number) {
+        if (level >= 17) {
+            return 6;
+        }
+        if (level >= 13) {
+            return 5;
+        }
+        if (level >= 9) {
+            return 4;
+        }
+        if (level >= 5) {
+            return 3;
+        }
+
+        return 2;
+    }
+}
